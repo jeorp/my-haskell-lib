7359375afe73f749a3f30f2059a1bbab6b809933
jeorp <jeorpqxx@gmail.com>
2022-02-04 15:51:06 +0900

    update

--- FunctionalUtility.hs
+++ FunctionalUtility.hs
@@ -1,4 +1,12 @@
- 
+
+import Control.Monad (when, unless)
+
 zip_ :: [a] -> (a -> b) -> [(a, b)]
 zip_ (a : xs) f = (a, f a) : zip_ xs f
 zip_ [] _ = []
+
+whenM :: Monad m => m Bool -> m () -> m ()
+whenM mb action = mb >>= flip when action
+
+unlessM :: Monad m => m Bool -> m () -> m ()
+unlessM mb action = mb >>= flip unless action
