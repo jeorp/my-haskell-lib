f453948d98f185db3a188d735ce26fb6b3e9bb17
jeorp <jeorpqxx@gmail.com>
2022-02-07 03:19:56 +0900

    update

--- FunctionalUtility.hs
+++ FunctionalUtility.hs
@@ -1,6 +1,14 @@
 
 import Control.Monad (when, unless)
 
+foldlC :: (b -> a -> b) -> (b -> Bool) -> b -> [a] -> b
+foldlC cf pred b (x:xs) = 
+  let cal = cf b x
+  in if pred cal
+    then foldlC cf pred cal xs 
+    else cal 
+foldlC cf pred b [] = b 
+
 zipSecond :: [a] -> (a -> b) -> [(a, b)]
 zipSecond (a : xs) f = (a, f a) : zipSecond xs f
 zipSecond [] _ = []
