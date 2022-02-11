7d52d32358dbb3f082d713ce335909118f7a5863
jeorp <jeorpqxx@gmail.com>
2022-02-12 03:55:41 +0900

    update

--- FunctionalUtility.hs
+++ FunctionalUtility.hs
@@ -18,3 +18,27 @@ whenM mb action = mb >>= flip when action
 
 unlessM :: Monad m => m Bool -> m () -> m ()
 unlessM mb action = mb >>= flip unless action
+
+or_implyl :: [a] -> (a -> a -> Bool) -> (a -> Bool)
+or_implyl xs f = \a -> or $ f a <$> xs 
+
+and_implyl :: [a] -> (a -> a -> Bool) -> (a -> Bool)
+and_implyl xs f = \a -> and $ f a <$> xs
+
+or_implyr :: [a] -> (a -> a -> Bool) -> (a -> Bool)
+or_implyr xs f = \a -> or $ flip f a <$> xs 
+
+and_implyr :: [a] -> (a -> a -> Bool) -> (a -> Bool)
+and_implyr xs f = \a -> and $ flip f a <$> xs
+
+or_eq :: Eq a => [a] -> (a -> Bool)
+or_eq xs = or_implyl xs (==)
+
+and_eq :: Eq a => [a] -> (a -> Bool)
+and_eq xs = and_implyl xs (==)
+
+or_neq :: Eq a => [a] -> (a -> Bool)
+or_neq xs = or_implyl xs (/=)
+
+and_neq :: Eq a => [a] -> (a -> Bool)
+and_neq xs = and_implyl xs (/=)
